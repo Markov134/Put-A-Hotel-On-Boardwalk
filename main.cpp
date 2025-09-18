6d4c8869976a8f47a5c45a6f19eb48d09272a6f6
Markov134 <stephanmarokvski@gmail.com>
2025-09-18 15:06:23 -0500

    Added a header file and in the header file is a templated circular linked list. In the main there it reads from a text file and displays the contents on the screen.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 
 #include "src/factorial.hpp"
+#include "src/MonopolyBoard.h"
+#include <string>
+#include <fstream>
+
+// No Benchmarking Needed
+// Make it a Generic Linked List
 
 int main() {
-  std::cout << "The factorial of 5 is " << factorial(5) << std::endl;
+  std::string location;
+  CircularlyLinkedList<std::string> steps;
+  std::ifstream FileIn("monopoly_spaces.txt");
+
+  if (!FileIn.is_open()) {
+    std::cout << "File could not be opened" << std::endl;
+    return 1;
+  }
+
+  while (std::getline(FileIn, location)) {
+    steps.push_back(location);
+  }
+
+  FileIn.close();
+  steps.print();
 }
---
+++ src/MonopolyBoard.h
@@ -0,0 +1,89 @@
+//
+// Created by Steph on 9/16/2025.
+//
+#include <iostream>
+#include <string>
+
+#ifndef MONOPOLYBOARD_H
+#define MONOPOLYBOARD_H
+
+
+template <typename T>
+class CircularlyLinkedList {
+  private:
+    // nested Node class omitted here; identical to the SinglyLinkedList Node class
+    class Node {
+        public:
+        T elem;
+        Node *next;
+
+        Node(const T &element, Node *nxt = nullptr) : elem(element), next(nxt) {}
+    };
+    // instance variables of the CircularlyLinkedList
+    int sz{0};                                           // number of entries in the list
+    Node* tail{nullptr};                                 // the node storing last element of the list
+
+  public:
+    CircularlyLinkedList() {}                            // Constructs an empty list
+
+    // ---------- access functions ----------
+    int size() const { return sz; }                      // Returns the number of elements stored
+    bool empty() const { return sz == 0; }               // Tests whether the list is empty.
+    T& front() { return tail->next->elem; }              // Live reference to first element
+    const T& front() const { return tail->next->elem; }  // Const reference to first element
+    T& back() { return tail->elem; }                     // Live reference to last element
+    const T& back() const { return tail->elem; }         // Const reference to last element
+
+    // ---------- update functions ----------
+
+    // Inserts element at the beginning of the list
+    void push_front(const T& elem) {
+        if (sz == 0) {
+            tail = new Node(elem);
+            tail->next = tail;                           // link to itself circularly
+        } else {
+            tail->next = new Node(elem, tail->next);     // splice new node after the tail
+        }
+        sz++;
+    }
+
+    // Inserts element at the end of the list
+    void push_back(const T& elem) {
+        push_front(elem);                                // start by inserting at front of list
+        tail = tail->next;                               // then rotate newest node to be tail
+    }
+
+    // Removes the first element of the list
+    void pop_front() {
+        Node* head = tail->next;
+        if (head == tail)
+            tail = nullptr;                              // must be the only node left
+        else
+            tail->next = head->next;
+        delete head;
+        sz--;
+    }
+
+    // Rotates the first element to the back of the list
+    void rotate() {
+        if (tail != nullptr)                             // if empty, do nothing
+            tail = tail->next;                           // otherwise, the old head becomes the new tail
+    }
+
+    void print() {
+        if (!tail) {
+            std::cout << "Empty List" << std::endl;
+        }
+
+        Node* current = tail->next;
+
+        do {
+            std::cout << current->elem << std::endl;
+            current = current->next;
+        } while (current != tail->next);
+    }
+};
+
+
+
+#endif //MONOPOLYBOARD_H
